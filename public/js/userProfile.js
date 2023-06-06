ace34b9bfe3fd7b1d4ea258374211d91368a1058
kelvinsjesus <kelvins.jesus@hotmail.com>
2023-06-05 21:24:04 -0300

    Adiciona telas de view e edit do perfil do usuário

---
+++ public/js/userProfile.js
@@ -0,0 +1,38 @@
+const avatar = document.getElementById('img-avatar');
+const banner = document.getElementById('img-banner');
+const userData = document.getElementById('userData');
+
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
+
+let user;
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log(username);
+    fetch(`/api/users/${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function(result) {
+        result.json().then(json => {
+            user = json[0];
+
+            loadProfile(user);
+        })
+    })
+});
+
+function loadProfile(user) {
+    avatar.src = user.avatarUrl;
+    banner.src = user.bannerUrl;
+
+    userData.innerHTML = `
+        <span>${user.name}</span>
+        <span>@${user.username}</span>
+        <span>${user.status}</span>
+        <span>Bio: ${user.bio}</span>
+        <span>Local: ${user.location}</span>
+        <span>Desde de: ${user.createdAt}</span>
+    `;
+}
