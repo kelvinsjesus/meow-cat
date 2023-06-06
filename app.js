b9bc7bdf1563a85ae957d6182c7463fc14e181cc
kelvinsjesus <kelvins.jesus@hotmail.com>
2023-06-05 22:17:06 -0300

    Deletando arquivos e configurando as rotas

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ process.env.AMBIENTE_PROCESSO = "desenvolvimento";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3000 : 8080;
 
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
 var userRouter = require("./src/routes/users");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
+var petRouter = require("./src/routes/pets");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,11 +18,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/users", userRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter)
+app.use("/api", indexRouter);
+app.use("/api/users", userRouter);
+app.use("/pets", petRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
